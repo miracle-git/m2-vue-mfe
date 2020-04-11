202981ff4d459d08bbf7fa5d9d2eb83067091f3e
miracle-git <hmiinyu@sina.com>
2020-04-11 18:49:49 +0800

    feat(): update the nested menu

--- m2.mfe.app1/src/router/index.js
+++ m2.mfe.app1/src/router/index.js
@@ -7,11 +7,15 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '',
-    redirect: '/list'
+    redirect: '/list1'
   },
   {
-    path: '/list',
-    component: () => import('views/list')
+    path: '/list1',
+    component: () => import('views/list1')
+  },
+  {
+    path: '/list2',
+    component: () => import('views/list2')
   },
   {
     path: '/:id/detail',
