e2e588057e63070c1e4eee72354b4b4bcac46776
miracle-git <hmiinyu@sina.com>
2020-04-11 13:40:20 +0800

    feat(): update the app script and json path

--- m2.app.portal/public/app.env.js
+++ m2.app.portal/public/app.env.js
@@ -1,7 +1,7 @@
 // 提供基于各自业务领域为框架所需的环境配置
 window.__mfe__ = {
   // 业务数据下发的api
-  apis: 'http://localhost:8080/apps.conf.json',
+  apis: '/apps.conf.json',
   // 可以对象形式传多个下发的api
   // apis: { apps: './apps.conf.json' },
   // 验权的自定义函数(根据用户权限点配置和应用配置做匹配，但系统提供默认的验证规则)
