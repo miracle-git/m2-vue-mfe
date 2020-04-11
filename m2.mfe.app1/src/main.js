85c594ae7e80bc553455eadcf844d979f438bbe3
miracle-git <hmiinyu@sina.com>
2020-04-11 19:07:11 +0800

    feat(): add the  config

--- m2.mfe.app1/src/main.js
+++ m2.mfe.app1/src/main.js
@@ -4,10 +4,11 @@ import { lifecycle } from 'm2-vue'
 import App from './views/app'
 import router from './router'
 import store from './store'
+import env from './config/env.conf'
 import i18n from './locales/i18n'
 import plugins from './plugins'
 
-const { bootstrap, mount, unmount } = lifecycle(App, { router, store, i18n, plugins, data: {} })
+const { bootstrap, mount, unmount } = lifecycle(App, { router, store, env, i18n, plugins, data: {} })
 
 // 挂载应用(非乾坤环境)
 window.__POWERED_BY_QIANKUN__ || mount()
--- m2.mfe.portal/src/render.js
+++ m2.mfe.portal/src/render.js
@@ -3,6 +3,7 @@ import { renderApp } from 'm2-vue'
 import App from './views/app'
 import router from './router'
 import store from './store'
+import env from './config/env.conf'
 import i18n from './locales/i18n'
 import plugins from './plugins'
 
@@ -11,6 +12,7 @@ export default function({ appContent, loading } = {}) {
     el: '#container',
     router,
     store,
+    env,
     i18n,
     plugins,
     appContent,
